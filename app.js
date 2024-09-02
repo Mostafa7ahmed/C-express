8285ee68c27c51dc14edb160cdfef4f5a557754b
Mostafa7ahmed <mohamedbesso876@gmail.com>
2024-09-02 04:49:28 +0300

    uptate

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express = require('express')
 const app = express()
-const port = 3002
+const port = 3005
+const mongoose = require('mongoose');
 
 app.get('/', (req, res) => {
     res.sendFile("./Views/index.html", {root: __dirname});
 })
 
-app.listen(port, () => {
-  console.log(`http://localhost:${port}/`)
-})
+
+
+mongoose.connect("mongodb+srv://Mostafa:sBmKCieTDlV6zeSf@cluster0.xr7ev.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+  .then(() => {
+    app.listen(port, () => {
+        console.log(`http://localhost:${port}/`)
+      })
+      
+  })
+  .catch((err) => {
+    console.error('Database connection error:', err);
+  });
