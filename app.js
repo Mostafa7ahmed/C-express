afe89a9bbe8bdc333b784fd8f8c601bf6c0741d2
Mostafa7ahmed <mohamedbesso876@gmail.com>
2024-09-02 15:59:06 +0300

    get Data

--- app.js
+++ app.js
@@ -1,42 +1,45 @@
-const express = require('express')
-const app = express()
-const port = 3005
-const mongoose = require('mongoose');
+const express = require("express");
+const app = express();
+const port = 3005;
+const mongoose = require("mongoose");
 app.use(express.urlencoded({ extended: true }));
-const MyData = require("./Models/myDataSchema")
+const MyData = require("./Models/myDataSchema");
+app.set("view engine", "ejs");
+app.get("/", (req, res) => {
+  MyData.find()
+    .then((data) => {
+      res.render("index", { mytitle: "home page" , arr:data });
 
-app.get('/', (req, res) => {
-    res.sendFile("./Views/index.html", {root: __dirname});
-})
-
-
+    })
+    .catch((err) => {console.error(err)});
+});
 
 app.get("/send.html", (req, res) => {
-    res.send("<h1>تم الارسال </h1>");
-})
+  res.send("<h1>تم الارسال </h1>");
+});
 
-mongoose.connect("mongodb+srv://Mostafa:sBmKCieTDlV6zeSf@cluster0.xr7ev.mongodb.net/all-data?retryWrites=true&w=majority&appName=Cluster0")
+mongoose
+  .connect(
+    "mongodb+srv://Mostafa:sBmKCieTDlV6zeSf@cluster0.xr7ev.mongodb.net/all-data?retryWrites=true&w=majority&appName=Cluster0"
+  )
   .then(() => {
     app.listen(port, () => {
-        console.log(`http://localhost:${port} /`)
-      })
-      
+      console.log(`http://localhost:${port} /`);
+    });
   })
   .catch((err) => {
-    console.error('Database connection error:', err);
+    console.error("Database connection error:", err);
   });
 
-
-
 app.post("/", (req, res) => {
-    const mydata = new MyData(req.body);
-     console.log(req.body )
-    mydata .save()
-    .then( () => {
+  const mydata = new MyData(req.body);
+  console.log(req.body);
+  mydata
+    .save()
+    .then(() => {
       res.redirect("/send.html");
     })
-    .catch( err => {
+    .catch((err) => {
       console.log(err);
     });
-
-  })
+});
